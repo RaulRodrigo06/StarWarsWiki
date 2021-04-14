ba1bc4b89c5ab94e31502d8159899796996dbe92
RaulRodrigo06 <raulrodrigo6@gmail.com>
2021-04-14 15:28:50 -0300

    Update Character Bloc
    Remoção de algumas linhas de código não utilizadas do character_bloc

--- lib/blocs/character_bloc.dart
+++ lib/blocs/character_bloc.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
 import 'package:desafio_flutter/api.dart';
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:desafio_flutter/models/characters.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class CharacterBloc implements BlocBase {
   Api api;
@@ -18,16 +15,6 @@ class CharacterBloc implements BlocBase {
 
   CharacterBloc() {
     api = Api();
-    /*
-    SharedPreferences.getInstance().then((prefs) {
-      if (prefs.getKeys().contains("character")) {
-        character = json.decode(prefs.getString("character")).map((k, v) {
-          return MapEntry(k, Character.fromJson(v));
-        }).cast<String, Character>();
-        _characterController.add(character);
-      }
-    });
-    */
     list();
   }
 
@@ -37,17 +24,8 @@ class CharacterBloc implements BlocBase {
     }
     character += await api.nexPage();
     _characterController.sink.add(character);
-
-    //_saveFav();
   }
 
-/*
-  void _saveFav() {
-    SharedPreferences.getInstance().then((prefs) {
-      prefs.setString("character", json.encode(character));
-    });
-  }
-*/
   @override
   void dispose() {
     _characterController.close();
