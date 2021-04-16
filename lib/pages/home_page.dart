745f5591da54d545bf942884a9d810f3ff5de8ac
RaulRodrigo06 <raulrodrigo6@gmail.com>
2021-04-16 13:36:55 -0300

    Correção Carregamento
    Correção no ícone de carregamento da homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -43,7 +43,7 @@ class Home extends StatelessWidget {
             } else {
               return Center(
                 child: CircularProgressIndicator(
-                  backgroundColor: Colors.white,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.yellow),
                 ),
               );
             }
